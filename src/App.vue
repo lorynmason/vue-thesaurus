f69076d70f429ba9f6cdcf50372ce4265a10e5d9
Loryn Mason <masonloryn@gmail.com>
2019-02-23 14:29:26 -0700

    Setup search component

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,19 @@
 <template>
   <div id="app">
     <Header />
+    <Search />
   </div>
 </template>
 
 <script>
 import Header from './components/Header.vue'
+import Search from './components/Search.vue'
 
 export default {
   name: 'app',
   components: {
-    Header
+    Header,
+    Search
   }
 }
 </script>
---
+++ src/components/Search.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <input placeholder="find synonyms"/> 
+    <button>SEARCH</button> 
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Search'
+}
+</script>
+
+<style scoped>
+div {
+  margin: 10px auto;
+}
+input, button {
+  margin: 0 5px;
+}
+
+</style>
