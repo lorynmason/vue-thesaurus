d211b86180863129c743115dee8d4c86b38c7326
Loryn Mason <masonloryn@gmail.com>
2019-02-23 14:01:22 -0700

    Setup header compontent

---
+++ src/components/Header.vue
@@ -0,0 +1,21 @@
+<template>
+  <header>
+    <img alt="logo" src="../assets/dino.png">    
+    <h1>thesaurus</h1>
+  </header>
+</template>
+
+<script>
+export default {
+  name: 'Header',
+  props: {
+    
+  }
+}
+</script>
+
+<style scoped>
+h1 {
+  margin: 40px 0 0;
+}
+</style>
