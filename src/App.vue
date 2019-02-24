52427b2060b6928afb7e7ed702087661a1696c23
Loryn Mason <masonloryn@gmail.com>
2019-02-23 21:13:13 -0700

    Style CardContainer

--- src/App.vue
+++ src/App.vue
@@ -30,16 +30,20 @@ export default {
     async search(){
       this.results = []
       const response = await fetch(`https://www.dictionaryapi.com/api/v3/references/thesaurus/json/${this.word}?key=${key}`)
-      const results = await response.json()
-      results.map(result => {
-        this.results.push({
-          id: result.meta.id,
-          speech: result.fl,
-          syns: result.meta.syns[0],
-          def: result.shortdef[0],
-          word: result.hwi.hw
+      if(response.ok){
+        const results = await response.json()
+        results.map(result => {
+          this.results.push({
+            id: result.meta.id,
+            speech: result.fl,
+            syns: result.meta.syns[0],
+            def: result.shortdef[0],
+            word: result.hwi.hw
+          })
         })
-      })
+      } else {
+        console.log('error')
+      }
     }
   }
 }
@@ -49,8 +53,10 @@ export default {
 body {
   text-align: center;
   color: #000000;
-  background: #2a2a2b;
+  background: #202020;
   margin: 0;
+  width: 100vw;
+  overflow-x: hidden;
 }
 
 .search {
